b09ce26eea26fc8c1d7fcb263a95951b08808f90
Ömer Biltekin <omerbiltekin_@outlook.com>
2021-02-14 15:27:00 +0300

    WebAPI

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -10,6 +10,7 @@ namespace Business.Abstract
     public interface ICarService
     {
         IDataResult<List<Car>> GetAll();
+        IResult Add(Car car);
         IResult Update(Car car);
         IResult Delete(Car car);
         IDataResult<Car> GetById(int id);
--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -14,11 +14,18 @@ namespace Business.Concrete
     {
 
         ICarDal _carDal;
+
         public CarManager(ICarDal carDal)
         {
             _carDal = carDal;
         }
 
+        public IResult Add(Car car)
+        {
+            _carDal.Add(car);
+
+            return new SuccessResult(Messages.CarAdded);
+        }
 
         public IResult Delete(Car car)
         {
@@ -29,10 +36,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Car>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
-            {
-                return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour == 22)
+            //{
+            //    return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
+            //}
 
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.CarListed);
         }
